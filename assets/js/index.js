434ab19b4dfbaa4cfc35b11f2bca0647e8fad117
markzhuo <markzhuo1@163.com>
2020-09-03 14:45:09 +0800

    index over

---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+$(function () {
+    getUserInof()
+
+    var layer = layui.layer
+
+    // 点击按钮，实现退出功能
+    $('#btnLogout').on('click', function () {
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            // 1. 清空本地存储token
+            localStorage.removeItem('token')
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+            // 关闭 confim 询问框
+            layer.close(index)
+        })
+    })
+})
+
+// 获取用户的基本信息
+function getUserInof() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+
+// 渲染用户的头像
+function renderAvatar(user) {
+    // 1. 获取昵称或用户名
+    var name = user.nickname || user.username
+    // 2. 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 3. 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        // 3.1 渲染图片头像，并显示头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide() // 隐藏文字头像
+    } else {
+        // 3.2 渲染文本头像
+        $('.layui-nav-img').hide() // 隐藏图片头像
+        var first = name[0].toUpperCase()
+        // 显示文字头像的显示内容，并显示
+        $('.text-avatar').html(first).show()
+    }
+}
